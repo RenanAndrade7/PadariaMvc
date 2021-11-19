925c53b90dd04c6cc231155190c31ca7bc132b87
Renan Andrade <renan.automation@gmail.com>
2021-11-19 19:47:40 -0300

    inicio

---
+++ PadariaWebMvc/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PadariaWebMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PadariaWebMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Atendemos diversos tipos de produtos a sua escolha.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Pagina de Contatos";
+            ViewData["Email"] = "padaria@gmail.com";
+            ViewData["Telefone"] = "996442641";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PadariaWebMvc/Data/PadariaWebMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PadariaWebMvc.Models;
+
+namespace PadariaWebMvc.Data
+{
+    public class PadariaWebMvcContext : DbContext
+    {
+        public PadariaWebMvcContext (DbContextOptions<PadariaWebMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PadariaWebMvc.Models.Client> Client { get; set; }
+    }
+}
